af7387151dbd1d46592e4d088660eaae0e675bbe
sridivya <129477222+sridivya2129@users.noreply.github.com>
2023-06-20 17:35:52 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,93 @@
+let element1 = document.getElementById("1");
+
+
+
+let element2 = document.getElementById("2");
+
+let element3 = document.getElementById("3");
+let element4 = document.getElementById("4");
+let element5 = document.getElementById("5");
+let element6 = document.getElementById("6");
+let element7 = document.getElementById("7");
+let element8 = document.getElementById("8");
+
+
+element1.addEventListener("click", function(){
+    
+    element1.style.backgroundColor = "#ffffa1";
+    element2.style.backgroundColor = "#ffffff";
+    element3.style.backgroundColor = "#ffffff";
+    element4.style.backgroundColor = "#ffffff";
+    element5.style.backgroundColor = "#ffffff";
+    element6.style.backgroundColor = "#ffffff";
+    element8.style.backgroundColor = "#ffffff";
+});
+
+
+
+
+element2.addEventListener("click", function(){
+    element1.style.backgroundColor = "#ffffff";
+    element2.style.backgroundColor = "#ffffa1";
+    element3.style.backgroundColor = "#ffffff";
+    element4.style.backgroundColor = "#ffffff";
+    element5.style.backgroundColor = "#ffffff";
+    element6.style.backgroundColor = "#ffffff";
+    element8.style.backgroundColor = "#ffffff";
+    
+});
+element3.addEventListener("click", function(){
+    element1.style.backgroundColor = "#ffffff";
+    element2.style.backgroundColor = "#ffffff";
+    element3.style.backgroundColor = "#ffffa1";
+    element4.style.backgroundColor = "#ffffff";
+    element5.style.backgroundColor = "#ffffff";
+    element6.style.backgroundColor = "#ffffff";
+    element8.style.backgroundColor = "#ffffff";
+    
+});
+element4.addEventListener("click", function(){
+    element1.style.backgroundColor = "#ffffff";
+    element2.style.backgroundColor = "#ffffff";
+    element3.style.backgroundColor = "#ffffff";
+    element4.style.backgroundColor = "#ffffa1";
+    element5.style.backgroundColor = "#ffffff";
+    element6.style.backgroundColor = "#ffffff";
+    element8.style.backgroundColor = "#ffffff";
+    
+    
+});
+element5.addEventListener("click", function(){
+    element1.style.backgroundColor = "#ffffff";
+    element2.style.backgroundColor = "#ffffff";
+    element3.style.backgroundColor = "#ffffff";
+    element4.style.backgroundColor = "#ffffff";
+    element5.style.backgroundColor = "#ffffa1";
+    element6.style.backgroundColor = "#ffffff";
+    element8.style.backgroundColor = "#ffffff";
+    
+});
+element6.addEventListener("click", function(){
+    element1.style.backgroundColor = "#ffffff";
+    element2.style.backgroundColor = "#ffffff";
+    element3.style.backgroundColor = "#ffffff";
+    element4.style.backgroundColor = "#ffffff";
+    element5.style.backgroundColor = "#ffffff";
+    element6.style.backgroundColor = "#ffffa1";
+    element8.style.backgroundColor = "#ffffff";
+    
+});
+
+
+
+element8.addEventListener("click", function(){
+    element1.style.backgroundColor = "#ffffff";
+    element2.style.backgroundColor = "#ffffff";
+    element3.style.backgroundColor = "#ffffff";
+    element4.style.backgroundColor = "#ffffff";
+    element5.style.backgroundColor = "#ffffff";
+    element6.style.backgroundColor = "#ffffff";
+    element8.style.backgroundColor = "#ffffa1";
+    
+});
+
